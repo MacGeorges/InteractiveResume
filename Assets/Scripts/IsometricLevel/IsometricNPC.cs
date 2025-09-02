65923a33cdb60a92b6d95a175106eef08692e410
Guy MacGeorges <fabien@themonkeyparadox.com>
2025-09-02 11:24:19 +0200

    Add NPC Idle, improve NPC animation controller

--- Assets/Scripts/IsometricLevel/IsometricNPC.cs
+++ Assets/Scripts/IsometricLevel/IsometricNPC.cs
@@ -26,11 +26,13 @@ public class IsometricNPC : MonoBehaviour
     private int health = 100;
     private bool isDead;
 
+    private float lastAction = 0;
+
     void Update()
     {
         graphics.LookAt(Camera.main.transform);
 
-        if(isDead || hitController.isHit)
+        if (isDead || hitController.isHit)
         {
             return;
         }
@@ -38,20 +40,29 @@ public class IsometricNPC : MonoBehaviour
         if (weaponController.Shoot())
         {
             animator.SetTrigger("Shoot");
+            lastAction = 0;
         }
-        else
+        else if((lastAction > 0.5) && (Vector3.Distance(transform.position, Camera.main.transform.position) > 5))
         {
             animator.SetTrigger("Walk");
             transform.Translate(graphics.forward * Time.deltaTime * speed, Space.Self);
         }
+        else if (lastAction > 0.25)
+        {
+            animator.SetTrigger("Idle");
+        }
+
+        lastAction += Time.deltaTime;
     }
 
     public void OnShot()
     {
+        lastAction = 0;
         health -= 25;
 
         if (health <= 0)
         {
+            Debug.Log("Die");
             animator.SetTrigger("Die");
             isDead = true;
 
@@ -59,6 +70,7 @@ public class IsometricNPC : MonoBehaviour
         }
         else
         {
+            Debug.Log("Hit");
             animator.SetTrigger("Hit");
         }
     }
