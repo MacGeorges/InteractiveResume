8a9fe483d24e6752dcc2c832363064f2be1b9bd9
Guy MacGeorges <fabien@themonkeyparadox.com>
2025-12-23 10:45:03 +0100

    Update project to LTS, add counter AutoSteer

--- Assets/Scripts/CarChaseLevel/CarChase_PlayerController.cs
+++ Assets/Scripts/CarChaseLevel/CarChase_PlayerController.cs
@@ -21,19 +21,36 @@ public class CarChase_PlayerController : MonoBehaviour
     [SerializeField]
     private float steeringSpeed;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         Vector2 moveValue = moveAction.ReadValue<Vector2>();
-        Vector3 newSteeringTargetPos = steeringTarget.localPosition + new Vector3(moveValue.x * steeringSpeed, 0, 0);
+
+        Vector3 newSteeringTargetPos;
+
+        if (moveValue.x != 0)
+        {
+            newSteeringTargetPos = steeringTarget.localPosition + new Vector3(moveValue.x * steeringSpeed, 0, 0);
+        }
+        else
+        {
+            float autosteerAngle = Vector3.Angle(policeCar.transform.forward, splineTarget.forward);
+            Vector3 cross = Vector3.Cross(policeCar.transform.forward, splineTarget.forward);
+            if (cross.y > 0) autosteerAngle = -autosteerAngle;
+
+            newSteeringTargetPos = steeringTarget.localPosition + new Vector3(autosteerAngle / 25, 0, 0);
+        }
 
         newSteeringTargetPos.x = Mathf.Clamp(newSteeringTargetPos.x, -5, 5);
+
+        if((newSteeringTargetPos.x == -5) || (newSteeringTargetPos.x == 5))
+        {
+            Debug.Log("Grinding");
+        }
 
         steeringTarget.localPosition = newSteeringTargetPos;
 
@@ -41,9 +58,8 @@ public class CarChase_PlayerController : MonoBehaviour
 
         policeCar.transform.LookAt(steeringTarget.position);
 
-        float angle = Vector3.Distance(policeCar.transform.position, steeringTarget.position) - 25;
-        //Debug.Log("angle: " + angle);
-        policeCar.Drift((angle > 2));
+        float driftAngle = Vector3.Distance(policeCar.transform.position, steeringTarget.position) - 25;
+        policeCar.Drift((driftAngle > 2));
 
         Camera.main.fieldOfView = Mathf.Clamp(Mathf.Lerp(Camera.main.fieldOfView, 60 * (moveValue.y + 1), Time.deltaTime), 40, 80);
     }
